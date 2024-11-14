de19dadd5a10067ee74ad69e269bab70ed2510aa
websitesByPatrick <98294337+websitesByPatrick@users.noreply.github.com>
2024-11-13 19:04:17 -0600

    heading mobile first update

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <div className="bg-slate-600 flex p-5 m-4">
-      <div className="flex flex-col w-3/12 items-center content-center">
+    <div className="bg-slate-600 flex flex-col items-center">
+      <div className="flex flex-row content-center">
         <Image
           alt="Photo of Marica Nicholas"
           src="/marica.jpg"
@@ -14,21 +14,22 @@ const Header = () => {
         />
       </div>
 
-      <div className="flex flex-col w-6/12 justify-center">
+      <div className="flex flex-col content-center">
         <div>
-          <h2 className="text-3xl p-1">Counseling Services</h2>
-          <h1 className="text-4xl p-1">Marica Nicholas, LPC-Associate</h1>
-          <p className="text-2xl p-1">
-            Supervised by: Ana Valenzuela, LPC-Supervisor
+          <h2 className="text-2xl p-1 text-center">Counseling Services</h2>
+          <h1 className="text-2xl p-1 text-center">Marica Nicholas, LPC-Associate</h1>
+          <p className="text-xl p-1 mt-4 text-center">
+            Supervised by: 
           </p>
-          <p className="text-xl p-1">
-            Located in Tomball, Texas
+          <p className="text-xl p-1 text-center">
+            Ana Valenzuela, LPC-Supervisor
           </p>
+          <p className="text-xl p-1 mt-4 text-center">Located in Tomball, Texas</p>
         </div>
       </div>
 
-      <div className="w-3/12">
-        <Image src="/header.jpg" width="300" height="300" alt="Mental Health"/>
+      <div className="flex my-4">
+        <Image src="/header.jpg" width="300" height="300" alt="Mental Health" />
       </div>
     </div>
   );
